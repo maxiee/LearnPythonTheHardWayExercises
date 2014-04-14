8e6ab328490a9823c6ca0c718299f48a86fff975
Maxiee <maxieewong@gmail.com>
2014-04-14 14:48:28 +0800

    ex5-8

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Maxiee'
+my_age = 24
+my_height = 180 #cm
+my_weight = 85 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kg herry." % my_weight
+print "Actually that's a little heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." %my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+        my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "Amazing! I got the same value as the author!"
