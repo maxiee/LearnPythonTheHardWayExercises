bb1ebad7644e4ef06cc532049049e686d49c337c
Maxiee <maxieewong@gmail.com>
2014-04-23 13:25:29 +0800

    use pickle to handle saved document

--- myGame/main.py
+++ myGame/main.py
@@ -6,16 +6,16 @@ print "****************************************"
 
 if myfile.isSaveExist():
     print "Now load the saving document..."
-    savedDocument = open("save.dat")
-    myName = savedDocument.readline().strip()
-    print "Welcome back, %s." % myName
+    data = myfile.readMySave()
+    print "Welcome back, %s." % data['name']
 else:
     print "It seems the first time you play this game..."
-    savedDocument = open("save.dat",'w')
     print "Now we'll create a new profile for you..."
     print "What's your name?"
     myName = raw_input(">")
     print "Good! Hello %s. Let's dive into the world of Maxiee!" % myName
-    savedDocument.write(myName + '\n')
+    data = {}
+    data['name'] = myName
+    myfile.updateMySave(data);
 
 print "Boring unpromising graduate life..."
--- myGame/myfile.py
+++ myGame/myfile.py
@@ -1,4 +1,13 @@
 from os import path
+import pickle
+
+saveDocPath = path.expandvars("$HOME/.config/myGameSaved.dat")
 
 def isSaveExist():
-    return path.exists("save.dat")
+    return path.exists(saveDocPath)
+
+def readMySave():
+    return pickle.load(open(saveDocPath))
+
+def updateMySave(data):
+    pickle.dump(data, open(saveDocPath, 'w'))
