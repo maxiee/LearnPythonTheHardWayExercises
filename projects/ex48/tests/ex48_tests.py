a2b258caf005a60db0060bb6a885854ba1544573
Maxiee <maxieewong@gmail.com>
2014-04-30 10:47:15 +0800

    add a test to ex49

--- projects/ex48/tests/ex48_tests.py
+++ projects/ex48/tests/ex48_tests.py
@@ -1,11 +1,13 @@
 from nose.tools import *
-import NAME
+from ex48 import parser
 
-def setup():
-    print "SETUP!"
-
-def teardown():
-    print "TEAR_DOWN!"
-
-def test_basic():
-    print "I RAN!"
+def test_sentence():
+    sentence1 = "bear go east"
+    sentence2 = "princess kill bear"
+    words1 = parser.scan(sentence1)
+    words2 = parser.scan(sentence2)
+    s_testing = parser.parse_sentence(words1)
+    s = parser.Sentence(('noun','bear'),('verb','go'),('direction','east'))
+    assert_equal(s_testing.subject,s.subject)
+    assert_equal(s_testing.verb,s.verb)
+    assert_equal(s_testing.object,s.object)
